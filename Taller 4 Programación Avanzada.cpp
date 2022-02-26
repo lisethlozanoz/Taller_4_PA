cd8a0c88fb1da45b9c19c43927865bb39f7186ae
ncontrerasn <36159522+ncontrerasn@users.noreply.github.com>
2022-02-25 21:13:08 -0500

    Update Taller 4 Programación Avanzada.cpp

--- Taller 4 Programación Avanzada.cpp
+++ Taller 4 Programación Avanzada.cpp
@@ -7,28 +7,46 @@
 
 using namespace std; 
 
-void crearVector(int size); //Para crear e imprimir el vector inicial dinamicamente
+int* crearVector(int size); //Para crear e imprimir el vector inicial dinamicamente
 
-void procesoOrdenamiento(int size);//Ordenar el vector segun el metodo de las cubetas
+void procesoOrdenamiento(int size, int* vec);//Ordenar el vector segun el metodo de las cubetas
 
+int digit(int a, int b) {
+    return a / (int) pow(10, b - 1) % 10;
+}
+
+int numDigitos(int n){
+	int number_of_digits = 0;
+
+	do {
+	     ++number_of_digits; 
+	     n /= 10;
+	} while (n);
+	return number_of_digits;
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 int main(){
 	
 	int longitud;
+	int* vec;
+	
+	cout<<digit(1234, 5)<<endl;
+	
+	cout<<numDigitos(1234)<<endl;
 	
 	cout<<"Ingrese la longitud del arreglo unidimensional"<<endl<<endl;
 	cin>>longitud;
 	
-	crearVector(longitud);
+	vec = crearVector(longitud);
 	
-	procesoOrdenamiento(longitud);
+	procesoOrdenamiento(longitud, vec);
 }
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-void crearVector(int size){
+int* crearVector(int size){
 	
 	int* vec;
 	
@@ -38,63 +56,77 @@ void crearVector(int size){
 	
 	srand(time(NULL));
 	for(int i=0; i<size; i++) {
-		*(vec + i)=1 + rand()%100;
+		*(vec + i)=1 + rand()%1000;
 	}
 	
 	for(int i=0; i<size; i++){
 		cout<<" ["<<*(vec + i)<<"] ";
 	}
+	cout<<endl<<endl;
+	return vec;
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-void procesoOrdenamiento(int size){
+void procesoOrdenamiento(int size, int* vec){
 	
-	
+	cout<<endl;
 	/////////////////////////////////// Crear la matriz /////////////////////////////////////////
-	int* vec;
 	int* *mat;
 	mat=new int*[10];
 	for(int i=0;i<10;i++){
 		*(mat+i)=new int[size];
 	}
-	    
-	    
-	///////////////////////////// Crear un vector auxiliar ///////////////////////////////////////
-	int* vectorAux;
-	//vectorAux=new int*[10];
-	
-	
-    /////////////////////////////// Ordenamiento segun el primer digito /////////////////////////////
-	int indice,numero,exponente=3;
 	
 	for(int i=0;i<10;i++){
 		for(int j=0;j<size;j++){
-			indice=*(vec+i)%10;
-			*(*(mat+indice)+j)=*(vec+i);
+			*(*(mat+i)+j)=-1;
+		}
+	}
+		
+	    
+	for(int it=0;it<3;it++){
+		int dig=0;
+		int num = 0;
+		int ind = 0;
+		
+		for(int i=0;i<size;i++){
+			num = *(vec+i);
 			
-			cout<<*(*(mat+indice)+j)<<"\t";
+			dig = digit(num, it + 1);
 			
-			if(i!=0){
-				*(vectorAux+i)=*(vec+i)/exponente;
+			while(*(*(mat+dig)+ind)!=-1){
+				ind++;
 			}
 			
-			exponente --;
+			*(*(mat+dig)+ind)=num;
+			ind=0;
 		}
 		
-		cout<<endl;
-	}
-	
-	
-	/////////////////////////////// Trasladar los valores al vector  /////////////////////////////
-	for(int i=0;i<10;i++){
-		for(int j=0;j<size;j++){
-			*vec=*(*(mat+i)+j);
+		ind = 0;
+		for(int i=0;i<10;i++){
+			for(int j=0;j<size;j++){
+				//cout<<*(*(mat+i)+j)<<" ";
+				if(*(*(mat+i)+j) != -1){
+					*(vec + ind) = *(*(mat+i)+j);
+					ind++;
+				}
+			}
+			//cout<<endl;
+		}
+		
+		cout<<"iteracion "<< it<<": "<<endl;
+			for(int i=0; i<size; i++){
+			cout<<" ["<<*(vec + i)<<"] ";
+		}
+		ind = 0;
+		cout<<endl<<endl;
+		
+		for(int i=0;i<10;i++){
+			for(int j=0;j<size;j++){
+				*(*(mat+i)+j)=-1;
+			}
 		}
 	}
+	
 }
-
-
-
-
-
