d804bd256115b671a671b6834963fee772ce09ed
lisethlozanoz <99454251+lisethlozanoz@users.noreply.github.com>
2022-02-28 09:41:58 -0500

    Add files via upload
    Ya cambié la creación de la matriz para que se cree dentro del for grande de la función procesoOrdenamiento y que así se cumpla la condición de crearla dinámicamente en cada ordenamiento, pero no cambié la parte del vector. No entiendo por qué tendría que crear el vector nuevamente en cada paso si ya se hizo al principio.
    
    Para cumplir con esta condición tendría que crear el vector y borrarlo en cada paso? Y si hago esto no perdería los valores iniciales del vector?

---
+++ Liseth Lozano - Taller 4 final.cpp
@@ -0,0 +1,129 @@
+//Tengo que usar la funcion pow, delete
+
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>
+#include <math.h>
+
+using namespace std; 
+
+int* crearVector(int size); //Para crear e imprimir el vector inicial dinamicamente
+
+void procesoOrdenamiento(int size, int* vec);//Ordenar el vector segun el metodo de las cubetas
+
+int digit(int a, int b) {
+    return a / (int) pow(10, b - 1) % 10;
+}
+
+int numDigitos(int n){
+	int numeroDigitos = 0;
+
+	do {
+	     ++numeroDigitos; 
+	     n /= 10;
+	} while (n);
+	return numeroDigitos;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+int main(){
+	
+	int longitud;
+	int* vec;
+	
+	cout<<"Ingrese la longitud del arreglo unidimensional"<<endl<<endl;
+	cin>>longitud;
+	
+	vec = crearVector(longitud);
+	
+	procesoOrdenamiento(longitud, vec);
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+int* crearVector(int size){
+	
+	int* vec;
+	
+	vec=new int[size];
+	
+	cout<<"El vector generado es:"<<endl<<endl;
+	
+	srand(time(NULL));
+	for(int i=0; i<size; i++) {
+		*(vec + i)=1 + rand()%1000;
+	}
+	
+	for(int i=0; i<size; i++){
+		cout<<" ["<<*(vec + i)<<"] ";
+	}
+	cout<<endl<<endl;
+	return vec;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void procesoOrdenamiento(int size, int* vec){
+	
+	cout<<endl;
+	    
+	for(int it=0;it<3;it++){
+		int dig=0;
+		int num = 0;
+		int ind = 0;
+		
+		int* *mat;
+	    mat=new int*[10];
+	    for(int i=0;i<10;i++){
+		    *(mat+i)=new int[size];
+	    }
+	
+	
+	    for(int i=0;i<10;i++){
+		    for(int j=0;j<size;j++){
+			    *(*(mat+i)+j)=-1;
+		    }
+	    }
+		
+		
+		for(int i=0;i<size;i++){
+			num = *(vec+i);
+			
+			dig = digit(num, it + 1);
+			
+			while(*(*(mat+dig)+ind)!=-1){
+				ind++;
+			}
+			
+			*(*(mat+dig)+ind)=num;
+			ind=0;
+		}
+		
+		ind = 0;
+		for(int i=0;i<10;i++){
+			for(int j=0;j<size;j++){
+				//cout<<*(*(mat+i)+j)<<" ";
+				if(*(*(mat+i)+j) != -1){
+					*(vec + ind) = *(*(mat+i)+j);
+					ind++;
+				}
+			}
+			//cout<<endl;
+		}
+		
+		cout<<"Ordenamiento "<< it+1<<": "<<endl;
+			for(int i=0; i<size; i++){
+			cout<<" ["<<*(vec + i)<<"] ";
+		}
+		
+		ind = 0;
+		cout<<endl<<endl;
+		
+		for(int i=0;i<10;i++){
+			for(int j=0;j<size;j++){
+				*(*(mat+i)+j)=-1;
+			}
+		}
+	}	
+}
